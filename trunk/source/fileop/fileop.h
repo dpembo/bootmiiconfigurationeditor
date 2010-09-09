210a39ef7d0c90efad50b55068b7f949ebb151c3
google@dpembo.ukfsn.org <google@dpembo.ukfsn.org@2a849c44-c264-11de-a009-bd819dffa471>
2010-09-09 06:19:44 +0000

    v2.6 changes

---
+++ trunk/source/fileop/fileop.h
@@ -0,0 +1,30 @@
+/****************************************************************************
+ * WiiMC
+ * Tantric 2009-2010
+ *
+ * fileop.h
+ *
+ * File operations
+ ****************************************************************************/
+
+
+#include <gccore.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAX_DEVICES 10
+
+typedef struct
+{
+	char name[100];
+	char mount[10];
+	int type;
+	DISC_INTERFACE* interface;
+	sec_t sector;
+} DEVICE_STRUCT;
+
+extern DEVICE_STRUCT part[2][MAX_DEVICES];
+
+void MountAllDevices();
+void UnmountAllDevices();
